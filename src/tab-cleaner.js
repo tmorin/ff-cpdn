f67bf68644b811bae83ab1a6acfc5aa48c77bac7
tmorin <thibault@morin.io>
2018-12-18 15:22:41 +0100

    fix: service workers were not properly unregistered

--- src/tab-cleaner.js
+++ src/tab-cleaner.js
@@ -23,16 +23,12 @@
         try {
             const registrations = await navigator.serviceWorker.getRegistrations();
             console.log('clear serviceWorker -', 'registrations', registrations.length);
-
             await Promise.all(registrations.map(async registration => {
-                console.log(registration.active, registration.scope);
-                if (registration.unregister === 'function') {
-                    console.log('clear serviceWorker -', 'unregister', registration);
-                    try {
-                        await registration.unregister();
-                    } catch (e) {
-                        console.warn('clear serviceWorker -', 'unable to unregister', e);
-                    }
+                console.log('clear serviceWorker - unregistering', registration);
+                try {
+                    await registration.unregister();
+                } catch (e) {
+                    console.warn('clear serviceWorker -', 'unable to unregister', e);
                 }
             }));
         } catch (e) {
