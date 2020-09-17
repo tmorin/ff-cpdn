dc849a0a7eb64cda32e7127499c26243c77c1617
tmorin <thibault@morin.io>
2020-09-17 10:38:13 +0200

    chore: upgrade dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,6 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common.js');
 
-
 module.exports = merge(common, {
     mode: 'production',
     module: {
