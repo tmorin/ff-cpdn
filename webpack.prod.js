63f882fb5db067cc14ec6f27e054052e7fe0eb61
tmorin <thibault@morin.io>
2022-11-18 20:45:46 +0100

    chore: update dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const {merge} = require('webpack-merge');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -13,7 +12,6 @@ module.exports = merge(common, {
     plugins: [
         new MiniCssExtractPlugin({
             filename: "[name].css",
-        }),
-        new OptimizeCssAssetsPlugin()
+        })
     ]
 });
